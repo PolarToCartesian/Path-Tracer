6685519cc940e1b67db99b602d59f19265aa5223
PolarToCartesian <24208000+PolarToCartesian@users.noreply.github.com>
2020-08-21 01:29:32 +0200

    Camera FOV

--- Path-Tracer/Include/Camera.hpp
+++ Path-Tracer/Include/Camera.hpp
@@ -2,8 +2,12 @@
 
 #include "Ray.hpp"
 
+#include <cmath>
+
 struct Camera {
-  Vec3f32 position;
+  Vec3f32 position{0.f, 0.f, 0.f};
+
+  float fov = 3.1415926f * 0.75f;
 
   Ray GenerateRandomRay(const std::uint32_t x, const std::uint32_t y, const std::uint32_t width,
                         const std::uint32_t height) noexcept;
--- Path-Tracer/Source/Camera.cpp
+++ Path-Tracer/Source/Camera.cpp
@@ -6,12 +6,14 @@ Ray Camera::GenerateRandomRay(const std::uint32_t x, const std::uint32_t y,
                               const std::uint32_t width, const std::uint32_t height) noexcept {
   Ray ray;
 
-  const float aspectRatio = width / (float)height;
+  const float apsectRatio = height / (float)width;
+  const float halffovTan  = std::tan(this->fov / 2.f);
 
   ray.origin = this->position;
 
-  ray.direction =
-      Vec3f32{2 * (x + 1u) / (float)width - 1.f, 2 * (height - y + 1u) / (float)height - 1.f, 1.f};
+  ray.direction.x = halffovTan * (2 * (         x + 1u) / (float)width  - 1.f) * apsectRatio;
+  ray.direction.y = halffovTan * (2 * (height - y + 1u) / (float)height - 1.f);
+  ray.direction.z = 1.f;
 
   ray.direction.Normalize();
 
