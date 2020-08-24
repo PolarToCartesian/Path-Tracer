b8c0afb31c27a9cbe1787a6351163b7e597ab145
Henry LE BERRE <hleberre64@gmail.com>
2020-08-24 22:01:46 +0200

    Fixed double declaration

--- Path-Tracer/Include/Camera.hpp
+++ Path-Tracer/Include/Camera.hpp
@@ -1,15 +1,13 @@
 #pragma once
 
-#include "Ray.hpp"
-
 #include <cmath>
+
+#include "Ray.hpp"
 
 struct Camera {
   Vec3f32 position{0.f, 0.f, 0.f};
 
   float fov = 3.1415926f * 0.75f;
-
-  float fov;
 
   Camera() = delete;
   Camera(const Vec3f32& position, const float fov);
--- Path-Tracer/Source/Camera.cpp
+++ Path-Tracer/Source/Camera.cpp
@@ -7,11 +7,11 @@ Ray Camera::GenerateRandomRay(const std::uint32_t x, const std::uint32_t y,
   Ray ray;
 
   const float apsectRatio = height / (float)width;
-  const float halffovTan  = std::tan(this->fov / 2.f);
+  const float halffovTan = std::tan(this->fov / 2.f);
 
   ray.origin = this->position;
 
-  ray.direction.x = halffovTan * (2 * (         x + 1u) / (float)width  - 1.f) * apsectRatio;
+  ray.direction.x = halffovTan * (2 * (x + 1u) / (float)width - 1.f) * apsectRatio;
   ray.direction.y = halffovTan * (2 * (height - y + 1u) / (float)height - 1.f);
   ray.direction.z = 1.f;
 
